1caee7ce6246242357f0bd1a3f7fa7531509432f
Pablo Soto <pabletesglez@gmail.com>
2021-11-17 23:06:36 +0100

    join rehecho

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 13:23:32 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/10/13 14:16:19 by psoto-go         ###   ########.fr       */
+/*   Updated: 2021/11/17 23:06:07 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,24 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char		*aux;
 	size_t		count;
 	size_t		i;
+	size_t		len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	aux = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	count = 0;
-	i = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	aux = (char *)malloc(len + 1 * sizeof(char));
 	if (!aux)
 		return (NULL);
-	while (count < ft_strlen(s1))
+	count = 0;
+	i = 0;
+	while (count < len && s1[count])
 	{
 		aux[count] = s1[count];
 		count++;
 	}
-	while (i < ft_strlen(s2))
-	{
-		aux[count] = s2[i];
-		count++;
-		i++;
-	}
+	while (count < len && s2[i])
+		aux[count++] = s2[i++];
 	aux[count] = '\0';
+	free((char *) s1);
 	return (aux);
 }
